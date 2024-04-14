7aee58b165dc3a3da4e11bff15b4c794e86a811c
Vignesh <nvignesh.india@gmail.com>
2024-04-14 10:01:58 +0530

    2nd commit

--- src/hellowordprogram/Main.java
+++ src/hellowordprogram/Main.java
@@ -1,8 +1,15 @@
 package hellowordprogram;
 
+import java.lang.System.Logger;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello! world.");
+		System.out.println("Please! enter your Name");
+		FetchName fetchName=new FetchName(null);
+		Scanner scanner=new Scanner(System.in);
+		fetchName.setName(scanner.next());
+		System.out.println("Hello! "+fetchName.getName());
 	}
 
 }
