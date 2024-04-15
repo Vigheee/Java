aca83a61299bc2be2f081c5f266965304d0be5ef
Vignesh <nvignesh.india@gmail.com>
2024-04-15 08:39:30 +0530

    added name with existing hello

---
+++ src/hellowordprogram/FetchName.java
@@ -0,0 +1,21 @@
+package hellowordprogram;
+
+public class FetchName {
+	String userName;
+
+	public FetchName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+
+}
--- src/hellowordprogram/Main.java
+++ src/hellowordprogram/Main.java
@@ -1,8 +1,13 @@
 package hellowordprogram;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello! world.");
+		System.out.println("Please enter your name");
+		Scanner scanner=new Scanner(System.in);
+		FetchName fetchName=new FetchName(scanner.next());
+		System.out.println("Hello! "+ fetchName.getUserName());
 	}
 
 }
